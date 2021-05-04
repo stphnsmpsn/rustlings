065f526300ac2d16028070e259b742ac8a24ae07
Steve Sampson <mail@stephensampson.dev>
2021-05-04 13:25:42 -0300

    Complete Section 5: Move Semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,12 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
+// I AM DONE 2021-05-04 by stphnsmpsn
 
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,12 +17,13 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut dst = Vec::new();
+    dst.copy_from_slice(vec);
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    dst.push(22);
+    dst.push(44);
+    dst.push(66);
 
-    vec
+    dst
 }
