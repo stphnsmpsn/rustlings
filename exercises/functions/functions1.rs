a8c754828a8a769607a51244a1b747750cc4762f
Steve Sampson <mail@stephensampson.dev>
2021-05-04 13:25:15 -0300

    Complete Section 2: Functions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,10 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE 2021-05-04 by stphnsmpsn
 
 fn main() {
     call_me();
 }
+
+fn call_me() {}
