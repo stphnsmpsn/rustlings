612520d9e52ab3f685e5854827a07931b8b090f1
Steve Sampson <mail@stephensampson.dev>
2021-05-05 20:48:27 -0300

    Complete Section 17: Tests

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,12 +6,12 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE 2021-05-04 by stphnsmpsn
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(true);
     }
 }
