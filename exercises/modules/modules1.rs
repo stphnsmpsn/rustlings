bb0ea0ef84cbdac950497332da08ed431d5d963b
Steve Sampson <mail@stephensampson.dev>
2021-05-04 21:04:57 -0300

    Complete Section 9: Modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,10 +1,10 @@
 // modules1.rs
 // Make me compile! Execute `rustlings hint modules1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE 2021-05-04 by stphnsmpsn
 
 mod sausage_factory {
-    fn make_sausage() {
+    pub fn make_sausage() {
         println!("sausage!");
     }
 }
